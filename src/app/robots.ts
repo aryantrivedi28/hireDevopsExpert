6d382cfbe6cdedf2dd334939b5b06c22ea58e9b1
kunal <ks1140175@gmail.com>
2026-07-14 02:34:59 +0530

    contact updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/styleguide"],
     },
-    sitemap: "https://hiredevopsexpert.com/sitemap.xml",
+    sitemap: "https://www.hiredevopsexpert.com/sitemap.xml",
   };
 }
